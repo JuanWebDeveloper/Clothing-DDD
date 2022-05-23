507741fea6c843855186b28ac7175aa221843264
Juan Vargas <juandeveloper19@gmail.com>
2022-05-23 03:11:41 -0500

    [ADD] Get records of all events that are executed

--- src/main/java/com/ddd/clothing/inventory/Inventory.java
+++ src/main/java/com/ddd/clothing/inventory/Inventory.java
@@ -2,6 +2,7 @@ package com.ddd.clothing.inventory;
 
 // Sofka dependency import
 import co.com.sofka.domain.generic.AggregateEvent;
+import co.com.sofka.domain.generic.DomainEvent;
 
 // Entities import
 import com.ddd.clothing.inventory.entities.Provider;
@@ -10,6 +11,7 @@ import com.ddd.clothing.inventory.entities.Product;
 // Value objects import
 import com.ddd.clothing.inventory.valueObjects.InventoryID;
 
+import java.util.List;
 import java.util.Set;
 
 public class Inventory extends AggregateEvent<InventoryID> {
@@ -18,5 +20,12 @@ public class Inventory extends AggregateEvent<InventoryID> {
 
     public Inventory(InventoryID inventoryID) {
         super(inventoryID);
+    }
+
+    // Get Event Logs
+    public static Inventory from(InventoryID inventoryID, List<DomainEvent> events) {
+        Inventory inventory = new Inventory(inventoryID);
+        events.forEach(inventory::applyEvent);
+        return inventory;
     }
 }
